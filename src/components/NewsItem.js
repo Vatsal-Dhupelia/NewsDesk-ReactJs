3c5f6990da9434e9e4c08dbe53cb8bee728b494b
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-16 14:57:57 +0530

    NewsItems Display by Fetching API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,13 +7,13 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl?"https://images.hindustantimes.com/img/2021/12/16/1600x900/nasa_perseverance_rover_1639624935633_1639624935775.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
                 </div>
-</div>
             </div>
         )
     }
