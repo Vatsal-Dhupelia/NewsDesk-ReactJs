e9116501852865b48a78133fa7689fa48658b68a
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-16 12:46:13 +0530

    Building the Structure

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
     render() {
+        let {title, description, imageUrl, newsUrl} = this.props
         return (
-            <div>
-                This is a news item.
+            <div className='my-3'>
+                <div className="card" style={{width: "18rem"}}>
+                    <img src={imageUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                </div>
+</div>
             </div>
         )
     }
