c996cd1f783500e711edf0ed4713fe1af7b88902
Vatsal Dhupelia <85610288+Vatsal-Dhupelia@users.noreply.github.com>
2021-12-17 16:01:01 +0530

    Adding time, author and news source

--- src/components/News.js
+++ src/components/News.js
@@ -76,7 +76,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element)=>{
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                            <NewsItem title={element.title?element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 88):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                         </div>
                     })}
                 </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let {title, description, imageUrl, newsUrl} = this.props
+        let {title, description, imageUrl, newsUrl, author, date, source} = this.props
         return (
             <div className='my-3'>
                 <div className="card">
+                <span className='position-absolute top-0 translate-middle badge rounded-pill bg-danger' style={{left: '90%', zIndex: '1'}}>{source}</span>
                     <img src={!imageUrl?"https://images.hindustantimes.com/img/2021/12/16/1600x900/nasa_perseverance_rover_1639624935633_1639624935775.jpg":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
+                        <p className='card-text'><small className='text-muted'>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
